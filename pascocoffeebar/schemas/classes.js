2c227a7c3fd3ee0b9d6e84af037bc70d6ffbbd87
Claire Roberts <clairefinnie@gmail.com>
2022-11-19 11:20:43 -0500

    schema edits

---
+++ pascocoffeebar/schemas/classes.js
@@ -0,0 +1,43 @@
+const classes = {
+	title: 'Classes',
+	name: 'classes',
+	type: 'document',
+	fields: [
+		{
+			title: 'Class Name',
+			name: 'className',
+			type: 'string',
+		},
+		{
+			title: 'Date',
+			name: 'date',
+			type: 'date',
+			validation: (Rule) => Rule.required(),
+		},
+		{
+			title: 'Month',
+			name: 'month',
+			type: 'number',
+			options: {
+				hotspot: true,
+			},
+		},
+		{
+			title: 'Availability',
+			name: 'availability',
+			type: 'number',
+			validation: (Rule) => Rule.required().positive().max('4'),
+		},
+		{
+			title: 'Type',
+			name: 'type',
+			type: 'reference',
+			to: {
+				type: 'classType',
+			},
+			validation: (Rule) => Rule.required(),
+		},
+	],
+}
+
+export default classes
--- pascocoffeebar/schemas/schema.js
+++ pascocoffeebar/schemas/schema.js
@@ -1,11 +1,13 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator';
+import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type';
+import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-import drinks from './drinks';
-import category from './category';
+import drinks from './drinks'
+import category from './category'
+import classes from './classes'
+import classType from './classType'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -13,5 +15,5 @@ export default createSchema({
 	name: 'default',
 	// Then proceed to concatenate our document type
 	// to the ones provided by any plugins that are installed
-	types: schemaTypes.concat([drinks, category]),
-});
+	types: schemaTypes.concat([drinks, category, classes, classType]),
+})
